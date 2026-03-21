a7c8596db4986eab08fd72340a3fa484c3c55d07
masterSahil <7882mastersahil@gmail.com>
2026-03-21 16:26:43 +0530

    ui

--- frontend/src/pages/Admins/DeletedAdmins.jsx
+++ frontend/src/pages/Admins/DeletedAdmins.jsx
@@ -103,15 +103,15 @@ const SoftDeletedAdmins = () => {
 
       <main className={`flex-1 bg-slate-50 ${customScrollbar} flex flex-col`}>
         {uiLoader && (
-        <div className="fixed inset-0 bg-white/10 backdrop-blur-sm flex items-center justify-center z-999">
-          <div className="rounded-xl bg-white/70 shadow-xl px-6 py-5 flex items-center gap-3">
-            <RefreshCw className="animate-spin text-slate-700" size={22} />
-            <span className="text-sm font-semibold text-slate-800">
-              Loading...
-            </span>
+          <div className="fixed inset-0 bg-white/10 backdrop-blur-sm flex items-center justify-center z-999">
+            <div className="rounded-xl bg-white/70 shadow-xl px-6 py-5 flex items-center gap-3">
+              <RefreshCw className="animate-spin text-slate-700" size={22} />
+              <span className="text-sm font-semibold text-slate-800">
+                Loading...
+              </span>
+            </div>
           </div>
-        </div>
-      )}
+        )}
 
         {/* Mobile Header */}
         <div className="lg:hidden p-4 pb-0 flex justify-between items-center shrink-0">
