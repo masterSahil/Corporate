898c49747add79e1ef388128198c4c11fc8c3177
masterSahil <7882mastersahil@gmail.com>
2026-04-19 16:05:22 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: [process.env.FRONTEND, process.env.CUSTOM_DOMAIN ],
-    methods: ["GET", "POST", "OPTIONS"],
+    origin: [process.env.FRONTEND, process.env.CUSTOM_DOMAIN ]
 }));
 app.use('/', userRoutes); 
 app.use('/', rewardRoutes); 
