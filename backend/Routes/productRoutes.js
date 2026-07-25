afb8426a8c72b9780bf95944a41bb67cc8d0d822
masterSahil <7882mastersahil@gmail.com>
2026-07-25 16:15:11 +0530

    secured products route

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -4,6 +4,7 @@ const productController = require("../controller/productController");
 const multer = require("multer");
 const {CloudinaryStorage} = require("multer-storage-cloudinary");
 const cloudinary = require("../config/Cloudinary");
+const {verifyUser} = require("../middleware/auth");
 
 const storage = new CloudinaryStorage({
     cloudinary,
@@ -14,23 +15,23 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({storage, limits: {fileSize: 1 * 1024 * 1024 }}); // 1 mb max
 
-router.get("/product", productController.getProduct);
-router.get("/product-single/:id", productController.getSingleProduct);
-router.get("/product-all", productController.getAllProduct);
-router.post("/product", upload.array('gallery', 10), productController.createProduct);
-router.put("/product/:id", upload.array('gallery', 10), productController.updateProduct);
-router.delete("/product-delete/:id", productController.hardDelete);
+router.get("/product", verifyUser, productController.getProduct);
+router.get("/product-single/:id", verifyUser, productController.getSingleProduct);
+router.get("/product-all", verifyUser, productController.getAllProduct);
+router.post("/product", verifyUser, upload.array('gallery', 10), productController.createProduct);
+router.put("/product/:id", verifyUser, upload.array('gallery', 10), productController.updateProduct);
+router.delete("/product-delete/:id", verifyUser, productController.hardDelete);
 
 // for doing soft delete
-router.put("/product-soft-delete/:id", productController.softDeleted);
+router.put("/product-soft-delete/:id", verifyUser, productController.softDeleted);
 
 // soft deleted
-router.get("/product-soft-delete-view", productController.softDeletedView);
+router.get("/product-soft-delete-view", verifyUser, productController.softDeletedView);
 
 // hard deleted
-router.post("/hard-delete-product/:id", productController.permanentDelete);
+router.post("/hard-delete-product/:id", verifyUser, productController.permanentDelete);
 
 // restore
-router.put("/product-restore/:id", productController.restoreProduct);
+router.put("/product-restore/:id", verifyUser, productController.restoreProduct);
 
 module.exports = router;
