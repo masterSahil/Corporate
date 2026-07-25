df4e18b0c26fab0ee807ce4463973b5c0435ea6c
masterSahil <7882mastersahil@gmail.com>
2026-07-25 16:18:35 +0530

    secured cart & orders

--- backend/Routes/cartRoutes.js
+++ backend/Routes/cartRoutes.js
@@ -2,27 +2,28 @@ const cartController = require("../controller/cartController");
 const checkout = require("../controller/checkout");
 const express = require('express');
 const router = express.Router();
+const {verifyUser} = require("../middleware/auth");
 
-router.get('/cart-all', cartController.getAllCarts);
-router.get('/cart', cartController.getCart);
-router.post('/cart', cartController.createCart);
-router.put('/cart/:id', cartController.updateCart);
-router.delete('/cart/:id', cartController.deleteCart);
+router.get('/cart-all', verifyUser, cartController.getAllCarts);
+router.get('/cart', verifyUser, cartController.getCart);
+router.post('/cart', verifyUser, cartController.createCart);
+router.put('/cart/:id', verifyUser, cartController.updateCart);
+router.delete('/cart/:id', verifyUser, cartController.deleteCart);
 
 // --- ORIGINAL CHECKOUT (Used if cart is 100% paid with points, total = ₹0) ---
-router.post('/checkout', checkout.checkout);
+router.post('/checkout', verifyUser, checkout.checkout);
 
 // --- NEW RAZORPAY ROUTES ---
-router.post('/create-razorpay-order', checkout.createRazorpayOrder);
-router.post('/verify-payment-and-checkout', checkout.verifyPaymentAndCheckout);
+router.post('/create-razorpay-order', verifyUser, checkout.createRazorpayOrder);
+router.post('/verify-payment-and-checkout', verifyUser, checkout.verifyPaymentAndCheckout);
 
 // orders all
-router.get('/orders', checkout.getAllOrders);
+router.get('/orders', verifyUser, checkout.getAllOrders);
 
 // orders by specific user
-router.post('/orders-user/:id', checkout.getUserOrders);
+router.post('/orders-user/:id', verifyUser, checkout.getUserOrders);
 
 // orders update
-router.patch('/order-status/:id', checkout.updateOrderStatus);
+router.patch('/order-status/:id', verifyUser, checkout.updateOrderStatus);
 
 module.exports = router;
