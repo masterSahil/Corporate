066f973fc822ac804c6e2408321a6fb4fca06782
masterSahil <7882mastersahil@gmail.com>
2026-07-25 16:33:34 +0530

    interceptor instead of manual headers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios';
+
+// interceptor to set token in every request:
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${sessionStorage.getItem("token")}`;
+  return config;
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
