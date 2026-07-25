87b35dcf71ef8fee2120b0a20ee76461e4ba07be
masterSahil <7882mastersahil@gmail.com>
2026-07-25 16:09:56 +0530

    secured reward routes

--- backend/Routes/rewardRoutes.js
+++ backend/Routes/rewardRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router();
 const rewardController = require("../controller/rewardController");
+const {verifyUser} = require("../middleware/auth");
 
-router.get("/reward", rewardController.getReward); // not deleted rewards
-router.get("/reward-deleted", rewardController.getSoftDeletedReward); // only deleted rewards
-router.get("/reward-all", rewardController.getAllReward); // both deleted & not deleted
-router.post("/reward", rewardController.createReward); // new reward
-router.put("/reward/:id", rewardController.updateReward); // update
-router.put("/reward-restore/:id", rewardController.restoringReward); // restore reward
-router.put("/reward-soft-delete/:id", rewardController.softDeleteReward); // soft delete
-router.post("/hard-delete-reward/:id", rewardController.permanentDelete); // remove
+router.get("/reward", verifyUser, rewardController.getReward); // active rewards
+router.get("/reward-deleted", verifyUser, rewardController.getSoftDeletedReward); // only deleted rewards
+router.get("/reward-all", verifyUser, rewardController.getAllReward); // both deleted & active
+router.post("/reward", verifyUser, rewardController.createReward); // new reward
+router.put("/reward/:id", verifyUser, rewardController.updateReward); // update
+router.put("/reward-restore/:id", verifyUser, rewardController.restoringReward); // restore reward
+router.put("/reward-soft-delete/:id", verifyUser, rewardController.softDeleteReward); // soft delete
+router.post("/hard-delete-reward/:id", verifyUser, rewardController.permanentDelete); // remove
 
 module.exports = router;
