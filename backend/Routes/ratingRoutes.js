106327cb4bdb68f624ef543dad8c4507293080e2
masterSahil <7882mastersahil@gmail.com>
2026-07-25 16:11:53 +0530

    secured ratings route

--- backend/Routes/ratingRoutes.js
+++ backend/Routes/ratingRoutes.js
@@ -1,11 +1,12 @@
 const ratingController = require("../controller/ratingController")
 const express = require("express")
 const router = express.Router();
+const {verifyUser} = require("../middleware/auth");
 
-router.get("/rating-all", ratingController.getAllRatings);
-router.get("/rating", ratingController.getRating);
-router.post("/rating", ratingController.createRating);
-router.put("/rating/:id", ratingController.updateRating);
-router.delete("/rating/:id", ratingController.deleteRating);
+router.get("/rating-all", verifyUser, ratingController.getAllRatings);
+router.get("/rating", verifyUser, ratingController.getRating);
+router.post("/rating", verifyUser, ratingController.createRating);
+router.put("/rating/:id", verifyUser, ratingController.updateRating);
+router.delete("/rating/:id", verifyUser, ratingController.deleteRating);
 
 module.exports = router;
